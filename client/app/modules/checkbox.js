8efed356b01e9a6189110f9859baa7f5e3ff7c89
Oleg <o.sas@aisnovations.com>
2017-01-25 14:09:52 +0300

    Application really read the public property of the user from the database - really works in any case

--- client/app/modules/checkbox.js
+++ client/app/modules/checkbox.js
@@ -2,7 +2,7 @@ angular.module('app.checkbox', [])
     .controller('checkboxCtrl', ['$scope', '$http', function($scope, $http) {
         $http.get('/getUserProfile')
         .then(public => {
-            console.log("public.data =  " + public.data.public);
+            console.log("public.data  =  " + public.data.public);
             $scope.public = public.data.public;
         });
     }]);
