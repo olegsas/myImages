6f8b83cbb488b2e6d8eaf7bdc44fb785d4f33e50
Oleg <o.sas@aisnovations.com>
2017-01-25 17:10:22 +0300

    Works good without comments!!!

--- client/app/modules/checkbox.js
+++ client/app/modules/checkbox.js
@@ -2,17 +2,17 @@ angular.module('app.checkbox', [])
     .controller('checkboxCtrl', ['$scope', '$http', function($scope, $http) {
         $http.get('/getUserProfile')
         .then(public => {
-            console.log("public.data  =  " + public.data.public);
+            // console.log("public.data  =  " + public.data.public);
             $scope.public = public.data.public;
         });
 
         $scope.update = function() {
-        console.log('====');
-         console.log('$scope.public = ' + $scope.public);
+        // console.log('====');
+        //  console.log('$scope.public = ' + $scope.public);
          $http.post('/updateProfile', {public:$scope.public})
          .then(public => {
-            console.log('+++');
-            console.log("public.data.public = "+public.data.public);
+            // console.log('+++');
+            // console.log("public.data.public = "+public.data.public);
              $scope.public = public.data.public;
          })          
          
